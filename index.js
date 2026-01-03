73e78382e7399f87d843180822f5f43925879c06
Chrischuka13 <divineprovidence30@gmail.com>
2026-01-03 14:54:31 +0100

    added

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require('cors');
 const PORT = process.env.PORT || 1500
 const mongoose = require("mongoose")
 const order = require('./routes/orderRoute')
 
 
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow only your frontend's origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  credentials: true // If you are sending cookies or authorization headers
+};
+
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use('/api/orders', order)
 
-app.get('/', (req, res)=>{
-    res.send('Server running')
-})
+app.get('/api/orders', (req, res) => {
+  res.json({ message: 'Orders data from the backend!' });
+});
 
 const startServer = async () => {
     try {
